f07e1e59fccd8bb0b0390e7596e6c0857f65a361
Azuresnow <31681010+Azuresnow@users.noreply.github.com>
2019-06-06 10:18:56 -0400

    Update home-page-content.php

--- template-parts/home-page-content.php
+++ template-parts/home-page-content.php
@@ -94,7 +94,20 @@ setup_postdata($post);
           <?php endforeach; ?>
         </ul>
       </div>
-      
+      <section class="ftco-animate">      
+
+            <section id="slider">
+  <input type="radio" name="slider" id="s1">
+  <input type="radio" name="slider" id="s2">
+  <input type="radio" name="slider" id="s3" checked>
+  <input type="radio" name="slider" id="s4">
+  <input type="radio" name="slider" id="s5">
+  <label for="s1" id="slide1"></label>
+  <label for="s2" id="slide2"> </label>
+  <label for="s3" id="slide3"></label>
+  <label for="s4" id="slide4"></label>
+  <label for="s5" id="slide5"></label>
+</section>
       <?php get_template_part("template-parts/story-carousel", "student") ?>
       
       <div class="news-events">
