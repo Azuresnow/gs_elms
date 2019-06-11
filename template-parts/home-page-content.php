aec0f1155d93472bdf973302e5d443ecc535bb94
Azuresnow <31681010+Azuresnow@users.noreply.github.com>
2019-06-11 13:12:28 -0400

    Update home-page-content.php

--- template-parts/home-page-content.php
+++ template-parts/home-page-content.php
@@ -16,7 +16,7 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="/wp-content/themes/gs_elms/js/google-map.js"></script>
   <script src="/wp-content/themes/gs_elms/js/main.js"></script>
-    
+    <link rel="stylesheet" type="text/css" href="/wp-content/themes/gs_elms/"
 
 
 
